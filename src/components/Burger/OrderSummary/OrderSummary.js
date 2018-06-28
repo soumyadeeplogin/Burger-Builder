42d4c19e3007cd1e9528ad42228207e621bebb28
soumyadeeplogin <soumyadeeplogin@gmail.com>
2018-06-28 20:21:21 +0530

    Sync

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-import Aux from '../../../hoc/Auxuliary/Auxiliary'
+import Aux from '../../../hoc/Auxiliary/Auxiliary'
 import Button from '../../UI/Button/Button'
 
 class OrderSummary extends Component {
